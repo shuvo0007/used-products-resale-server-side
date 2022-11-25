84a20555962ed595c5537fb362846a511ec20b97
Md. Samiul Islam <samiul15-6903@diu.edu.bd>
2022-11-25 23:43:32 +0600

    update index

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const { ObjectID, ObjectId } = require("bson");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Service Review Server Running");
+});
+
+const uri =
+  "mongodb+srv://e-commerce-web:XHOhPWIrH44oP0th@cluster0.zfaesfn.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const userCollection = client.db("e-commerce").collection("laptop");
+    app.get("/laptop", async (req, res) => {
+      const query = {};
+      const cursor = userCollection.find(query);
+      const category = await cursor.toArray();
+      res.send(category);
+    });
+    app.post("/laptop", async (req, res) => {
+      const category = req.body;
+      const allCategory = await userCollection.insertOne(category);
+      res.send(allCategory);
+    });
+  } finally {
+  }
+}
+run().catch((error) => console.log(error));
+app.listen(port, () => {
+  console.log(`Service Review Server running on port ${port}`);
+});
