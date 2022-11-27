36205d0f1fbbfbe68fe137dc611753858ab3524d
Md. Samiul Islam <samiul15-6903@diu.edu.bd>
2022-11-27 17:10:00 +0600

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,35 @@ async function run() {
       const allCategory = await userCollection.insertOne(category);
       res.send(allCategory);
     });
+    app.get("/laptop/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const laptop = await userCollection.findOne(query);
+      res.send(laptop);
+    });
+    app.put("/laptop/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const laptop = req.body;
+      const option = { upset: true };
+      const updateLaptop = {
+        $set: {
+          advertised: laptop.advertised,
+        },
+      };
+      const result = await userCollection.updateOne(
+        filter,
+        updateLaptop,
+        option
+      );
+      res.send(result);
+    });
+    app.delete("/laptop/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
